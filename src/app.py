bbac766e37049457130a273885aad59ec1960dba
Codjoero <rownee2001@gmail.com>
2018-09-23 19:51:13 +0300

    chore(Manages the todo list): Tasks module linked to app.py
    This makes all list tasks operational with app.py
    
    [chore #160619626]

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
     """
     Gives user 3 chances to input the right username-password
-    combo, then creates a new account otherwise
+    then creates a new account otherwise
     """
     def user_check():
         count = 1
@@ -52,13 +52,131 @@ if __name__ == "__main__":
                 add_account(userName, password)
                 break
             elif login(userName, password):
-
                 break
-            
             else:
                 print("Wrong Username or Password!")
                 count += 1
+    """
+    Local task functions:
+    """
+    def create_local_task():
+        print("--------------------")
+        print("Let's Create a Task.")
+        print("--------------------")
+        task = str(input("Enter Task name: "))
+        create_task(task)
+
+    def delete_local_task():
+        if not todo_list:
+            print("You don't have any Task in your List!")
+        else:
+            print("-----------------------")
+            print("You're Deleting a Task.")
+            print("-----------------------")
+            print("Select Option:")
+            for index, item in enumerate(todo_list):
+                indent = str(index + 1)
+                print("%s. %s" % (indent.rjust(4), item))
+            count = 0
+            while True:
+                if count == 3:
+                    raise SystemExit('Too many wrong inputs! Exiting...')
+                else:
+                    try:
+                        task = int(input("Enter Task Number: "))
+                        if  0 < task <= len(todo_list):
+                            delete_task(todo_list[task - 1])
+                            break
+                        else:
+                            print("Select the right Task Number!")
+                    except ValueError:
+                        print("Oops! Select the right Task Number!")                
+                count += 1
+    def del_all_local_task():
+        if not todo_list:
+            print("You don't have any Task in your List!")
+        else:
+            count = 0
+            while True:
+                if count == 3:
+                    raise SystemExit('Too many wrong inputs! Exiting...')
+                else:
+                    confirm = input("Are you sure? Y or N: ")
+                    if confirm.upper() == 'Y':
+                        delete_all_tasks()
+                        break
+                    elif confirm.upper() == 'N':
+                        break
+                    else:
+                        print("Choose Y or N!")
+                        count += 1
     
+    def mark_local_finished():
+        if not todo_list:
+            print("You don't have any Task in your List!")
+        else:
+            print("------------------------------------")
+            print("You're Marking a task as [Finished].")
+            print("------------------------------------")
+            print("Select Option:")
+            for index, item in enumerate(todo_list):
+                indent = str(index + 1)
+                print("%s. %s" % (indent.rjust(4), item))
+            count = 0
+            while True:
+                if count == 3:
+                    raise SystemExit('Too many wrong inputs! Exiting...')
+                else:
+                    try:
+                        task = int(input("Enter Task Number: "))
+                        if  0 < task <= len(todo_list):
+                            mark_as_finished(todo_list[task - 1])
+                            break
+                        else:
+                            print("Select the right Task Number!")
+                    except ValueError:
+                        print("Oops! Select the right Task Number!")                
+                count += 1
+    
+    """
+    Enter User's profile
+    """
+    def user_profile():
+        display = ["Create Task", "Delete Task", "Delete all Tasks", "Mark a task finished"]
+        print()
+        print("==============")
+        print("Select Option:")
+        for i in range(4):
+            indent = str(i + 1)
+            print("%s. %s" % (indent.rjust(4), display[i]))
+
+        """
+        Making a selection
+        """
+        func_local = {
+            1 :create_local_task, 
+            2 :delete_local_task, 
+            3 :del_all_local_task, 
+            4 :mark_local_finished
+        }
+        count = 0
+        while True:
+            if count == 3:
+                raise SystemExit('Too many wrong inputs! Exiting...')
+            else:
+                try:
+                    selection = int(input("selection: "))
+                    if  0 < selection < 5:
+                        func_local[selection]()
+                        user_profile()
+                        break
+                    else:
+                        print("Make a selection 1 -- 4!")
+                except ValueError:
+                    print("Oops! Enter a number btn 1 and 4!")
+                count += 1
+
+
     #ENTRY INTO PROGRAM
     user = input("Welcome! Have an Account? Y or N: ")
     count = 1
@@ -68,7 +186,7 @@ if __name__ == "__main__":
 
         elif user.upper() == 'Y':
             user_check()
-            #more code
+            user_profile()
             break
         elif user.upper() == 'N':
             print("------------------------")
@@ -76,44 +194,9 @@ if __name__ == "__main__":
             print("------------------------")
             input_check()
             add_account(userName, password)
+            user_profile()
             break
         else:
             user = input("I didn't get you...: ")
             count += 1
         
-    """
-    Enter User's profile
-    """
-
-    display = ["Create Task", "Delete Task", "Delete all Tasks", "Mark a task finished"]
-
-    print("Select Option:")
-    for i in range(4):
-        indent = str(i + 1)
-        print("%s. %s" % (indent.rjust(4), display[i]))
-
-    """
-    Making a selection
-    """
-    func_select = {
-        1 :create_task, 
-        2 :delete_task, 
-        3 :delete_all_tasks, 
-        4 :mark_as_finished
-        }
-    count = 0
-    while True:
-        if count == 3:
-            raise SystemExit('Too many wrong inputs! Exiting...')
-        else:
-            selection = input("selection: ")
-            if selection.isdigit() and 0 < selection > 5:
-                func_select[selection]()
-                break
-            else:
-                print("Make a selection 1 -- 4!")
-                count += 1
-
-    
-
-  
--- src/tasks.py
+++ src/tasks.py
@@ -4,26 +4,25 @@
 todo_list = []
 
 # Write a function creates a task
-
 def create_task(task):
     """
     Adds the task (string value) to todo_list
     """
     task = str(task)
     todo_list.append(task)
+    print("%s added to List" %(task))
+    print("=====================")
 
 # Write a function deletes a task
-
-
 def delete_task(task):
     """
     Removes the specified task from the todo_list
     """
     todo_list.remove(task)
+    print("%s removed from List" %(task))
+    print("=====================")
 
 # Write a function that marks a task finished
-
-
 def mark_as_finished(task):
     """
     Append the string label '[finished]' at the end of the task 
@@ -32,17 +31,23 @@ def mark_as_finished(task):
     """
     l = []
     l.append(task)
-    if '[finished]' not in l:
+    
+    if '[finished]' in l:
+        print("Task already marked finished!")
+    else:
         l.append('[finished]')
         new_word = ''.join(l)
-        return new_word
-
+    
+    todo_list[todo_list.index(task)] = new_word
+    print("%s marked as finished" % task)
+    print("=====================")
+    
 # Write a function deletes all tasks
-
-
 def delete_all_tasks():
     """
     Empty the the todo_lsit
     """
     todo_list.clear()
+    print("All Tasks Deleted!")
+    print("=====================")
     
